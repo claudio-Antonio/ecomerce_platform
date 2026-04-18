5d7ff2cb77ba443832e0de9e3c09475a7ef9d2f5
claudio-Antonio <claudioanery@gmail.com>
2026-04-18 10:11:00 -0300

    Simplifying Redis implementation in microservices

--- auth-service/src/main/java/com/example/auth_service/repositories/jpa/UserRepository.java
+++ auth-service/src/main/java/com/example/auth_service/repositories/jpa/UserRepository.java
@@ -1,6 +1,8 @@
 package com.example.auth_service.repositories;
 
 import com.example.auth_service.domain.User;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
@@ -9,5 +11,9 @@ import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    @Cacheable(value = "users", key = "#email")
     UserDetails findByEmail(String email);
+
+    @CacheEvict(value = "users", key = "#email")
+    void deleteByEmail(String email);
 }
